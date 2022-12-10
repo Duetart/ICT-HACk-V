c424987529e2d8f4d753af2c98d57d4a7d5c7648
vov4k <golovach.volodia@yandex.ru>
2022-12-10 17:56:37 +0300

    auth

---
+++ web/app/Http/Controllers/Auth/AdminAuthController.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Http\Requests\Auth\LoginRequest;
+
+class AdminAuthController extends Controller
+{
+    /**
+     * Display admin login view
+     *
+     * @return RedirectResponse
+     */
+    public function showLoginForm()
+    {
+        if (Auth::guard('admin')->check()) {
+            return redirect()->route('admin.dashboard');
+        } else {
+            return view('auth.adminLogin');
+        }
+    }
+
+    /**
+     * Handle an incoming admin authentication request.
+     *
+     * @param  \App\Http\Requests\Auth\LoginRequest  $request
+     * @return RedirectResponse
+     */
+    public function login(Request $request)
+    {
+        $this->validate($request, [
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+
+        if(auth()->guard('admin')->attempt([
+            'email' => $request->email,
+            'password' => $request->password,
+        ])) {
+            $user = auth()->user();
+
+            return redirect()->intended(url('/admin/dashboard'));
+        } else {
+            return redirect()->back()->withError('Credentials doesn\'t match.');
+        }
+    }
+
+    /**
+     * Destroy an authenticated session.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return RedirectResponse
+     */
+    public function logout(Request $request)
+    {
+        Auth::guard('admin')->logout();
+
+        $request->session()->invalidate();
+
+        $request->session()->regenerateToken();
+
+        return redirect('/');
+    }
+}

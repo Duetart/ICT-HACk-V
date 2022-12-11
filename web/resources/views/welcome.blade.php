4462a11644f0469a99675eaec5654eaa954a2054
vov4k <golovach.volodia@yandex.ru>
2022-12-11 03:25:37 +0300

    Revert "ЁЩЁ СТРАШНЕЕ"
    This reverts commit 4af6c86952ebfdbe3678ceb85a27b852fd1f0be1.

--- web/resources/views/welcome.blade.php
+++ web/resources/views/welcome.blade.php
@@ -1,38 +1,4 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <title>Laravel</title>
-
-        <!-- Fonts -->
-        <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
-
-        <!-- Styles -->
-
-        <style>
-            body {
-                font-family: 'Nunito', sans-serif;
-            }
-        </style>
-    </head>
-    <body>
-        @extends('layouts.app')
-        @section('content')
-
-
-    <main>
-
-    </main>
-    <floor>
-
-    </floor>
-        @endsection
-    </body>
-
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
-    <script src="../js/app.js"></script>
-</html>
+@extends('layouts.app')
+@section('content')
+    hello
+@endsection
